b9eba68b6f260d278d7be8b11ae49be88174c971
issdilshod <dilshod@themembers.com>
2022-08-24 23:37:19 +0300

    Task #36: Test 3

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'cors' => \App\Http\Middleware\Cors::class,
         'auth.custom' => \App\Http\Middleware\AuthenticateCustom::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
