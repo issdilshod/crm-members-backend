9a2f8238194978a0cc2c6b28f97eb7078d2e1860
issdilshod <dilshod@themembers.com>
2022-09-14 21:05:12 +0300

    test telegram user model

---
+++ app/Helpers/TeegramHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\API\TelegramUser;
+
+class TelegramHelper{
+
+    public static function getTelegramChatId($nickname){
+        $telegram_user = TelegramUser::where('username', $nickname)->first();
+        $chat_id = null;
+        if ($telegram_user!=null){
+            $telegram_user = $telegram_user->toArray();
+            $chat_id = $telegram_user['telegram_id'];
+        }
+        return $chat_id;
+    }
+}
