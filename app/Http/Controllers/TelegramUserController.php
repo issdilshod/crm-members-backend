5e345944d6bb506fa10552f65c89e08185a1eea0
issdilshod <dilshod@themembers.com>
2022-09-14 20:16:49 +0300

    test

--- app/Http/Controllers/TelegramUserController.php
+++ app/Http/Controllers/TelegramUserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class TelegramUserController extends Controller
 {
@@ -12,7 +11,9 @@ class TelegramUserController extends Controller
         $request = file_get_contents( 'php://input' );
         //$request = json_decode( $request, TRUE );
 
-        Storage::disk('local')->put('uploads/telegram.txt', $request);
+        $f = fopen("uploads/test.txt", "w");
+        fwrite($f, $request);
+        fclose($f);
     }
 
 }
