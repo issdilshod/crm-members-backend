601696a084abe104a71b8904b85d4d7bde1ac1d4
issdilshod <dilshod@themembers.com>
2022-09-14 20:32:12 +0300

    test

--- app/Http/Controllers/TelegramUserController.php
+++ app/Http/Controllers/TelegramUserController.php
@@ -2,18 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\API\TelegramUser;
+use App\Notifications\TelegramNotification;
+use Illuminate\Support\Facades\Notification;
 
 class TelegramUserController extends Controller
 {
 
     public function index(){
-        $request = file_get_contents( 'php://input' );
-        //$request = json_decode( $request, TRUE );
+        // commands
+        $commands = [
+            '/start' => 'Hello to platform bot!'
+        ];
 
-        $f = fopen("uploads/test.txt", "w");
-        fwrite($f, $request);
-        fclose($f);
+        $request = file_get_contents('php://input');
+        $request = json_decode( $request, TRUE );
+
+        // getting message
+        $request = $request['message'];
+
+        $entity = [
+            'telegram_id' => $request['from']['id'],
+            'is_bot' => $request['from']['is_bot'],
+            'first_name' => $request['from']['first_name'],
+            'username' => $request['from']['username'],
+            'language_code' => $request['from']['language_code'],
+            'message' => $request['text']
+        ];
+
+        $telegram_user = TelegramUser::where('telegram_id', $entity['telegram_id'])->first();
+        if ($telegram_user!=null){
+            TelegramUser::create($entity);
+        }
+
+        // send message response
+        $msg_response = 'If there are some news on platform, we will send message to you!';
+        if (isset($commands[$entity['text']])){
+            $msg_response = $commands[$entity['text']];
+        }
+        Notification::route('telegram', $entity['telegram_id'])
+                      ->notify(new TelegramNotification(['msg' => $msg_response]));
+
     }
 
 }
