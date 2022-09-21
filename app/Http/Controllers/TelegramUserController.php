1adc726d99b0de6d74be16aabb31c95b06a2d8ed
issdilshod <dilshod@themembers.com>
2022-09-21 18:53:58 +0300

    telegram user logic to service

--- app/Http/Controllers/TelegramUserController.php
+++ app/Http/Controllers/TelegramUserController.php
@@ -2,52 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\API\TelegramUser;
-use App\Notifications\TelegramNotification;
-use Illuminate\Support\Facades\Notification;
+use App\Services\TelegramUserService;
 
 class TelegramUserController extends Controller
 {
 
-    public function index(){
-        // commands
-        $commands = [
-            '/start' => 'Hello from platform.'
-        ];
+    public function index(TelegramUserService $telegramUserService){
+        $updates = $telegramUserService->getUpdates();
 
-        $request = file_get_contents('php://input');
-        $request = json_decode( $request, TRUE );
+        $entity = $telegramUserService->getEntity($updates['message']);
 
-        // getting message
-        $request = $request['message'];
+        $telegramUserService->createTelegramUser($entity);
 
-        $entity = [
-            'telegram_id' => $request['from']['id'],
-            'is_bot' => $request['from']['is_bot'],
-            'first_name' => $request['from']['first_name'],
-            'username' => $request['from']['username'],
-            'language_code' => $request['from']['language_code'],
-            'message' => $request['text']
-        ];
+        $msg_response = $telegramUserService->getResponse($entity);
 
-        $telegram_user = TelegramUser::where('telegram_id', $entity['telegram_id'])->first();
-        if ($telegram_user==null){
-            TelegramUser::create($entity);
-        }
-
-        // send message response
-        $msg_response = 'If there are some news on platform, we will send message to you!';
-        if (isset($commands[$entity['message']])){
-            $msg_response = $commands[$entity['message']];
-        }
-
-        $f = fopen('uploads/telegram.txt', 'w');
-        fwrite($f, $msg_response);
-        fclose($f);
-
-        Notification::route('telegram', $entity['telegram_id'])
-                      ->notify(new TelegramNotification(['msg' => $msg_response]));
-
+        $telegramUserService->sendResponse($entity, $msg_response);
     }
 
 }
---
+++ app/Services/TelegramUserService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\API\TelegramUser;
+use App\Notifications\TelegramNotification;
+use Illuminate\Support\Facades\Notification;
+
+class TelegramUserService {
+
+    public $commands = [
+        '/start' => 'Hello from platform.',
+        '/help' => 'Help section.',
+    ];
+
+    /**
+     * Return telegram updates
+     * 
+     * @return array
+     */
+    public function getUpdates() 
+    {
+        $request = file_get_contents('php://input');
+        $request = json_decode( $request, TRUE );
+    }
+
+    /**
+     * Return entity
+     * 
+     * @return array
+     */
+    public function getEntity($message)
+    {
+        return [
+            'telegram_id' => $message['from']['id'],
+            'is_bot' => $message['from']['is_bot'],
+            'first_name' => $message['from']['first_name'],
+            'username' => $message['from']['username'],
+            'language_code' => $message['from']['language_code'],
+            'message' => $message['text']
+        ];
+    }
+
+    /**
+     * Return telegram user by telegram id
+     * 
+     * @return void
+     */
+    public function createTelegramUser($entity)
+    {
+        $telegram_user = TelegramUser::where('telegram_id', $entity['telegram_id'])
+                                        ->first();
+        if ($telegram_user==null){
+            TelegramUser::create($entity);
+        }
+    }
+
+    /**
+     * Return message of respond
+     * 
+     * @return string
+     */
+    public function getResponse($entity)
+    {
+        $msg_response = 'If there are some news on platform, we will send message to you!';
+
+        if (isset($this->commands[$entity['message']])){
+            $msg_response = $this->commands[$entity['message']];
+        }
+
+        return $msg_response;
+    }
+
+    /**
+     * Send response to user from bot
+     * 
+     * @return void
+     */
+    public function sendResponse($entity, $msg)
+    {
+        Notification::route('telegram', $entity['telegram_id'])
+                      ->notify(new TelegramNotification(['msg' => $msg]));
+    }
+}
