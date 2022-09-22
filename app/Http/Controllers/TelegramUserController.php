c5dab2d174144807650c8e363b6f7615634421d0
issdilshod <dilshod@themembers.com>
2022-09-22 14:58:02 +0300

    refactoring updates

--- app/Http/Controllers/TelegramUserController.php
+++ app/Http/Controllers/TelegramUserController.php
@@ -11,10 +11,6 @@ class TelegramUserController extends Controller
 
         $updates = $telegramUserService->getUpdates();
 
-        $f = fopen('telegram_log/telegram'.date('YmdHis').'.txt', 'w');
-        fwrite($f, print_r($updates, true));
-        fclose($f);
-
         $entity = $telegramUserService->getEntity($updates['message']);
 
         $telegramUserService->createTelegramUser($entity);
