11121928a2348c2420922abe00ff867bee9bb1a1
issdilshod <dilshod@themembers.com>
2022-10-09 20:05:48 +0300

    routes by order

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,31 @@ use App\Http\Controllers\Helper\StateController;
 use App\Http\Controllers\Task\TaskController;
 use Illuminate\Support\Facades\Route;
 
+// helpers
 Route::resource('api/sic_code', SicCodeController::class)->middleware('auth.custom');
 Route::resource('api/state', StateController::class)->middleware('auth.custom');
 Route::resource('api/hosting', HostingController::class)->middleware('auth.custom');
-
 Route::resource('api/department', DepartmentController::class)->middleware('auth.custom');
 Route::resource('api/role', RoleController::class)->middleware('auth.custom');
-// Login
+
+// login
 Route::resource('api/user', UserController::class)->middleware('auth.custom');
 Route::get('api/is_auth', [UserController::class, 'is_auth'])->middleware('auth.custom');
 Route::post('api/login', [UserController::class, 'login']);
 Route::post('api/logout', [UserController::class, 'logout'])->middleware('auth.custom');
 Route::get('api/get_me', [UserController::class, 'get_me'])->middleware('auth.custom');
-// Activity
+Route::post('api/invite-via-email', [InviteUserController::class, 'via_email'])->middleware('auth.custom');
+Route::post('api/invite-via-telegram', [InviteUserController::class, 'via_telegram'])->middleware('auth.custom');
+Route::post('api/invite-check-token', [InviteUserController::class, 'check_token']);
+Route::post('api/invite-register', [UserController::class, 'invite_register']);
+Route::get('api/pending-users', [UserController::class, 'pending_users']);
+
+// activities
 Route::get('api/activity', [ActivityController::class, 'index'])->middleware('auth.custom');
 Route::get('api/activity/user/{uuid}', [ActivityController::class, 'by_user'])->middleware('auth.custom');
 Route::get('api/activity/entity/{uuid}', [ActivityController::class, 'by_entity'])->middleware('auth.custom');
 
+// directors
 Route::resource('api/director', DirectorController::class)->middleware('auth.custom');
 Route::get('api/director', [DirectorController::class, 'index'])->middleware('auth.custom');
 Route::get('api/director/{uuid}', [DirectorController::class, 'show'])->middleware('auth.custom');
@@ -44,18 +52,25 @@ Route::put('api/director-pending-update/{uuid}', [DirectorController::class, 'pe
 Route::put('api/director-accept/{uuid}', [DirectorController::class, 'accept'])->middleware('auth.custom');
 Route::put('api/director-reject/{uuid}', [DirectorController::class, 'reject'])->middleware('auth.custom');
 
+// companies
 Route::resource('api/company', CompanyController::class)->middleware('auth.custom');
+Route::get('api/company', [CompanyController::class, 'index'])->middleware('auth.custom');
+Route::get('api/company/{uuid}', [CompanyController::class, 'show'])->middleware('auth.custom');
+Route::post('api/company', [CompanyController::class, 'store'])->middleware('auth.custom');
+Route::put('api/company/{uuid}', [CompanyController::class, 'update'])->middleware('auth.custom');
+Route::delete('api/company/{uuid}', [CompanyController::class, 'destroy'])->middleware('auth.custom');
 Route::get('api/company-search/{search}', [CompanyController::class, 'search'])->middleware('auth.custom');
+Route::post('api/company-pending', [CompanyController::class, 'pending'])->middleware('auth.custom');
+Route::put('api/company-pending-update/{uuid}', [CompanyController::class, 'pending_update'])->middleware('auth.custom');
+Route::put('api/company-accept/{uuid}', [CompanyController::class, 'accept'])->middleware('auth.custom');
+Route::put('api/company-reject/{uuid}', [CompanyController::class, 'reject'])->middleware('auth.custom');
 
+// tasks
 Route::resource('api/task', TaskController::class)->middleware('auth.custom');
 
-Route::post('api/invite-via-email', [InviteUserController::class, 'via_email'])->middleware('auth.custom');
-Route::post('api/invite-via-telegram', [InviteUserController::class, 'via_telegram'])->middleware('auth.custom');
-Route::post('api/invite-check-token', [InviteUserController::class, 'check_token']);
-Route::post('api/invite-register', [UserController::class, 'invite_register']);
-Route::get('api/pending-users', [UserController::class, 'pending_users']);
-
-Route::post('api/telegram-hook', [TelegramUserController::class, 'index']);
-
+// notes
 Route::resource('api/note', NoteController::class)->middleware('auth.custom');
 Route::get('api/note_by_user', [NoteController::class, 'show_by_user'])->middleware('auth.custom');
+
+// telegram hook
+Route::post('api/telegram-hook', [TelegramUserController::class, 'index']);
