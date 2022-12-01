bcf01db3108d46fd5cbecde485506569b73002ea
issdilshod <dilshod@themembers.com>
2022-12-01 16:00:59 +0300

    test collection unique

--- app/Http/Controllers/Helper/PendingController.php
+++ app/Http/Controllers/Helper/PendingController.php
@@ -105,8 +105,8 @@ class PendingController extends Controller
         $companies = $companies->merge($director_related);
         $directors = $directors->merge($company_related);
 
-        $companies = $companies->groupBy('uuid');
-        $directors = $directors->groupBy('uuid');
+        $companies = $companies->unique('uuid'); $companies->all();
+        $directors = $directors->unique('uuid'); $directors->all();
 
         return ['directors' => $directors->toArray(), 'companies' => $companies->toArray()];
     }
